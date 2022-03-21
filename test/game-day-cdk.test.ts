f90c165fb8d5fb1a3a86e4e70a7df153257ff4af
agomez <agomezssp@gmail.com>
2022-03-21 10:51:34 -0300

    Initial commit

---
+++ test/game-day-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GameDayCdk from '../lib/game-day-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/game-day-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GameDayCdk.GameDayCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
